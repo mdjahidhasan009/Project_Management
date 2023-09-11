73efb148df738ce0823e20a9a6e45e8741ac0883
MONTASIM <mmamun@mymedicalhub.com>
2023-09-11 19:24:01 +0600

    [ADDED] home page footer

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function Home(props) {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div className="bg-default min-h-screen p-4 flex flex-col items-center justify-center lg:space-y-10 md:space-y-8 space-y-4 text-indigo-100 overscroll-none">
+        <div className="bg-default min-h-screen p-6 flex flex-col items-center justify-center lg:space-y-10 md:space-y-8 space-y-4 text-indigo-100 overscroll-none">
             <section className="flex flex-col items-center justify-center space-y-10">
                 <h1 className="text-center lg:text-7xl md:text-5xl text-3xl line font-bold">
                     Impossible alone.
@@ -24,6 +26,28 @@ function Home(props) {
                     </a>
                 </div>
             </section>
+
+            <footer className="flex lg:flex-row md:flex-row flex-col items-center justify-center gap-3 lg:pt-40 md:pt-28 pt-20">
+                <p className="text-sm text-gray-300 lg:text-center md:text-center text-center">© {currentYear} —
+                    <a href="https://github.com/montasim" className="text-[#2fffdc] ml-1" rel="noopener noreferrer" target="_blank">@montasim</a>
+                    <span> & </span>
+                    <a href="https://github.com/mdjahidhasan009" className="text-[#2fffdc] ml-1" rel="noopener noreferrer" target="_blank">@mdjahidhasan009</a>
+                </p>
+
+                <span id="socialMediaLink" className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    <a className="text-gray-400">
+                      <img alt="GitHub forks" src="https://img.shields.io/github/forks/mdjahidhasan009/Project_Management?style=social"/>
+                    </a>
+
+                    <a className="ml-3 text-gray-400">
+                      <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/mdjahidhasan009/Project_Management?style=social"/>
+                    </a>
+
+                    <a className="ml-3 text-gray-400">
+                      <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/mdjahidhasan009/Project_Management"/>
+                    </a>
+                </span>
+            </footer>
         </div>
     );
 }
