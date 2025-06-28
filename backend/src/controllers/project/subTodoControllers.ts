92b80bdcf240e8470ea77999bf7413ceead52a45
mdjahidhasan009 <jahid60012@gmail.com>
2025-06-28 22:10:11 +0600

    fixed types of subTodoControllers.ts

--- backend/src/controllers/project/subTodoControllers.ts
+++ backend/src/controllers/project/subTodoControllers.ts
@@ -1,7 +1,8 @@
 import { validationResult } from "express-validator";
+import { Response } from 'express';
 
 import Project from "../../models/Project";
-import {IExpressRequestWithUser} from "../../types";
+import {IExpressRequestWithUser, IProject, ISubTodo, ITodo} from "../../types";
 
 interface SubTodoRequest {
   todo: string;
@@ -18,44 +19,59 @@ interface SubTodoDoneRequest {
 // @route   POST api/project/todos/:projectId/todoId/:todoId
 // @desc    Add new sub todo
 // @access  Private
-const addNewSubTodo = async(req: IExpressRequestWithUser & { body: SubTodoRequest }, res: Response) => {
+const addNewSubTodo = async(req: IExpressRequestWithUser & { body: SubTodoRequest }, res: Response): Promise<void> => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-      return res.status(400).json({ 'error': 'Server Error' });
+      res.status(400).json({ 'error': 'Server Error' });
+      return;
     }
     try {
       const newTodo = {
-        user: req.user.id,
-        addedBy: req.user.id,
-        text: req.body.todo,
+        user: req?.user?.id,
+        addedBy: req?.user?.id,
+        text: req?.body?.todo,
       };
       await Project.update({ '_id': req.params.projectId , 'todos._id': req.params.todoId},
           { $push: { 'todos.$.subTodos': newTodo }});
       let updatedProject = await Project.findById(req.params.projectId)
           .populate('todos.user', 'username profileImage -_id')
           .populate('todos.subTodos.user', 'username profileImage -_id');
-      await res.json(updatedProject.todos);
+      if(!updatedProject || !updatedProject.todos || updatedProject.todos.length === 0) {
+        res.status(404).json({ 'message': 'No todos found in this project' });
+        return;
+      }
+      res.status(200).json(updatedProject.todos);
     } catch(error) {
       console.error(error);
-      return res.status(400).json({ 'error': 'Server Error' });
+      res.status(400).json({ 'error': 'Server Error' });
     }
 }
 
 // @route   PUT api/project/todos/:projectId/:todoId/:subTodoId
 // @desc    Edit an existing sub todo
 // @access  Private
-const editSubTodo = async(req: IExpressRequestWithUser & { body: SubTodoEditRequest }, res: Response) => {
+const editSubTodo = async(req: IExpressRequestWithUser & { body: SubTodoEditRequest }, res: Response): Promise<void> => {
     try {
       //Checking is the subTodo assigned to current user
-      let todos = await Project.findById(req.params.projectId)
+      let todos: IProject | null = await Project.findById(req.params.projectId)
           .select({ 'todos' : { $elemMatch: { _id: req.params.todoId }}})
       //as elemMatch does not work for nested element
-      let subTodoRequested = await todos.todos[0].subTodos.filter(subTodo =>
-          subTodo._id.toString() === req.params.subTodoId.toString());
-      subTodoRequested = subTodoRequested[0]; //as return array
-      let isAssignedToCurrentUser = subTodoRequested.user.toString() === req.user.id.toString();
-      if(!isAssignedToCurrentUser)
-        return res.status(400).json({ 'error': 'This sub todo does not assign to you.' });
+      if(!todos || !todos.todos || todos.todos.length === 0 || !todos.todos[0].subTodos) {
+        res.status(404).json({ 'error': 'No todos found in this project' });
+        return;
+      }
+      let subTodoRequested: ISubTodo | null = todos.todos[0].subTodos.filter(subTodo =>
+          subTodo?._id?.toString() === req.params.subTodoId.toString())[0];
+      if(!subTodoRequested) {
+        res.status(404).json({ 'error': 'No sub todo found' });
+        return;
+      }
+      // subTodoRequested = subTodoRequested[0]; //as return array
+      let isAssignedToCurrentUser = subTodoRequested?.user?.toString() === req?.user?.id.toString();
+      if(!isAssignedToCurrentUser) {
+        res.status(400).json({'error': 'This sub todo does not assign to you.'});
+        return;
+      }
 
       //Update subTodo
       await Project.updateOne({
@@ -73,29 +89,40 @@ const editSubTodo = async(req: IExpressRequestWithUser & { body: SubTodoEditRequ
 
       let project = await Project.findById(req.params.projectId)
           .populate('todos.user', 'username profileImage -_id')
-          .populate('todos.subTodos.user', 'username profileImage -_id')
-      await res.json(project.todos);
+          .populate('todos.subTodos.user', 'username profileImage -_id');
+      if(!project || !project.todos || project.todos.length === 0) {
+        res.status(404).json({ 'message': 'No todos found in this project' });
+        return;
+      }
+      res.status(200).json(project.todos);
     } catch(error) {
       console.error(error);
-      return res.status(400).json({ 'error': 'Server Error' });
+      res.status(400).json({ 'error': 'Server Error' });
     }
 }
 
 // @route   PUT api/project/toggle/todos/:projectId/:todoId/:subTodoId
 // @desc    Set sub todo done or incomplete
 // @access  Private
-const toggleIsSubTodoDone = async(req: IExpressRequestWithUser & { body: SubTodoDoneRequest }, res: Response) => {
+const toggleIsSubTodoDone = async(req: IExpressRequestWithUser & { body: SubTodoDoneRequest }, res: Response): Promise<void> => {
     try {
       //Checking is the subTodo assigned to current user
-      let todos = await Project.findById(req.params.projectId)
-          .select({ 'todos' : { $elemMatch: { _id: req.params.todoId }}})
+      let todos: IProject | null = await Project.findById(req.params.projectId)
+          .select({ 'todos' : { $elemMatch: { _id: req.params.todoId }}});
+      if(!todos || !todos.todos || todos.todos.length === 0 || !todos.todos[0]?.subTodos || todos.todos[0].subTodos.length === 0) {
+        res.status(404).json({ 'error': 'No todos found in this project' });
+        return;
+      }
       //as elemMatch does not work for nested element
-      let subTodoRequested = await todos.todos[0].subTodos.filter(subTodo =>
-          subTodo._id.toString() === req.params.subTodoId.toString());
-      subTodoRequested = subTodoRequested[0]; //as return array
-      let isAssignedToCurrentUser = subTodoRequested.user.toString() === req.user.id.toString();
-      if(!isAssignedToCurrentUser)
-        return res.status(400).json({ 'error': 'This sub todo does not assign to you.' });
+      let subTodoRequested: ISubTodo = todos.todos[0].subTodos.filter(subTodo =>
+          subTodo?._id?.toString() === req.params.subTodoId.toString())[0];
+
+      // subTodoRequested = subTodoRequested[0]; //as return array
+      let isAssignedToCurrentUser = subTodoRequested?.user?.toString() === req?.user?.id?.toString();
+      if(!isAssignedToCurrentUser) {
+        res.status(400).json({'error': 'This sub todo does not assign to you.'});
+        return;
+      }
 
       //Toggling isDone
       let isDone = req.body.isDone === 'true';
@@ -117,30 +144,43 @@ const toggleIsSubTodoDone = async(req: IExpressRequestWithUser & { body: SubTodo
 
       let project = await Project.findById(req.params.projectId)
           .populate('todos.user', 'username profileImage -_id')
-          .populate('todos.subTodos.user', 'username profileImage -_id')
-      await res.json(project.todos);
+          .populate('todos.subTodos.user', 'username profileImage -_id');
+      if(!project || !project.todos || project.todos.length === 0) {
+          res.status(404).json({ 'message': 'No todos found in this project' });
+          return;
+      }
+      res.status(200).json(project.todos);
     } catch(error) {
       console.error(error);
-      return res.status(400).json({ 'error': 'Server Error' });
+      res.status(400).json({ 'error': 'Server Error' });
     }
 }
 
 // @route   DELETE api/project/todos/:projectId/:todoId/:subTodoId
 // @desc    Delete a sub todo
 // @access  Private
-const deleteSubTodo = async(req: IExpressRequestWithUser, res: Response) => {
+const deleteSubTodo = async(req: IExpressRequestWithUser, res: Response): Promise<void> => {
     try {
       //Checking is the subTodo assigned to current user
       let todos = await Project.findById(req.params.projectId)
-          .select({ 'todos' : { $elemMatch: { _id: req.params.todoId }}})
+          .select({ 'todos' : { $elemMatch: { _id: req.params.todoId }}});
+      if(!todos || !todos.todos || todos.todos.length === 0 || !todos.todos[0].subTodos || todos.todos[0].subTodos.length === 0) {
+          res.status(404).json({ 'error': 'No todos found in this project' });
+          return;
+      }
       //as elemMatch does not work for nested element
-      let subTodoRequested = await todos.todos[0].subTodos.filter(subTodo =>
-          subTodo._id.toString() === req.params.subTodoId.toString());
-      if(subTodoRequested.length <= 0) return res.status(400).json({ 'error': 'No sub todo there' });
-      subTodoRequested = subTodoRequested[0]; //as return array
-      let isAssignedToCurrentUser = subTodoRequested.user.toString() === req.user.id.toString();
-      if(!isAssignedToCurrentUser)
-        return res.status(400).json({ 'error': 'This sub todo does not assign to you.' });
+      let subTodoRequested: ISubTodo | null = todos.todos[0].subTodos.filter(subTodo =>
+          subTodo?._id?.toString() === req?.params?.subTodoId?.toString())[0]
+      if(!subTodoRequested) {
+        res.status(400).json({ 'error': 'No sub todo there' });
+        return;
+      }
+      // subTodoRequested = subTodoRequested[0]; //as return array
+      let isAssignedToCurrentUser = subTodoRequested?.user?.toString() === req?.user?.id?.toString();
+      if(!isAssignedToCurrentUser) {
+        res.status(400).json({'error': 'This sub todo does not assign to you.'});
+        return;
+      }
 
       //deleting sub todo
       await Project.updateOne({
@@ -158,11 +198,15 @@ const deleteSubTodo = async(req: IExpressRequestWithUser, res: Response) => {
 
       let project = await Project.findById(req.params.projectId)
           .populate('todos.user', 'username profileImage -_id')
-          .populate('todos.subTodos.user', 'username profileImage -_id')
-      await res.json(project.todos);
+          .populate('todos.subTodos.user', 'username profileImage -_id');
+        if(!project || !project.todos || project.todos.length === 0) {
+          res.status(404).json({ 'message': 'No todos found in this project' });
+          return;
+        }
+      res.status(200).json(project.todos);
     } catch(error) {
       console.error(error);
-      return res.status(400).json({ 'error': 'Server Error' });
+      res.status(400).json({ 'error': 'Server Error' });
     }
 }
 
